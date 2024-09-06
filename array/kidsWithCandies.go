be8d2f6150d0c73cbdce7c1d0776be2fc6197f9d
Amir <razavi77amir@gmail.com>
2024-09-06 17:17:34 +0330

    feat-KidsWithCandies

---
+++ array/kidsWithCandies.go
@@ -0,0 +1,15 @@
+package Array
+
+func KidsWithCandies(candies []int, extraCandies int) []bool {
+	biggest := 0
+	for _, candy := range candies {
+		if candy > biggest {
+			biggest = candy
+		}
+	}
+	potential := make([]bool, len(candies))
+	for i, candy := range candies {
+		potential[i] = candy+extraCandies >= biggest
+	}
+	return potential
+}
--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
-)
 
-import (
 	Array "algo/array"
-
 )
+
 func main() {
 	// var a = Array.MergeAlternately("fghtd", "ds")
 	// fmt.Println(a)
-		var a = Array.GcdOfStrings("ABCABC", "ABC")
+	// 	var a = Array.GcdOfStrings("ABCABC", "ABC")
+	// fmt.Println(a)
+	var arr = []int{6, 5, 3, 5, 3}
+	var a = Array.KidsWithCandies(arr, 9)
 	fmt.Println(a)
+
 }
